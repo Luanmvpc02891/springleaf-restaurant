14ba1946f2db3c6710dcc3ae3407384593f6e017
maiquocbao666 <maiquocbao666@gmail.com>
2023-09-11 14:37:25 +0700

    edit user-header.module.ts

--- springleaf_restaurant/src/app/user/component/user-home/user-header/user-header.module.ts
+++ springleaf_restaurant/src/app/user/component/user-home/user-header/user-header.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HeaderRoutingModule } from './header-routing.module';
+import { HeaderRoutingModule } from './user-header-routing.module';
 
 
 @NgModule({
