6fc2b3bfb5dce975d4bd7a1ce10570beadda955b
maiquocbao666 <maiquocbao666@gmail.com>
2023-10-24 15:11:02 +0700

    Cập nhật đồng hồ

--- springleaf_restaurant/src/app/components/date-time/date-time.component.ts
+++ springleaf_restaurant/src/app/components/date-time/date-time.component.ts
@@ -11,7 +11,7 @@ export class DateTimeComponent {
   dateTimeWorker: Worker;
   currentTime: Date = new Date();
   hours: number[] = Array.from({ length: 12 }, (_, index) => index + 1);
-  seconds : number[] = Array.from({ length: 60 }, (_, index) => index + 1);
+  seconds: number[] = Array.from({ length: 60 }, (_, index) => index + 1);
   //hourDegrees: number = 0;
   //minuteDegrees: number = 0;
   //secondDegrees: number = 0;
@@ -25,24 +25,28 @@ export class DateTimeComponent {
     this.dateTimeWorker.postMessage('start');
 
     this.dateTimeWorker.onmessage = (event) => {
-     this.currentTime = event.data;
+      this.currentTime = event.data;
     };
   }
 
   get hourHandTransform() {
     const hours = this.currentTime.getHours() % 12;
-    const degrees = hours * 30 + this.currentTime.getMinutes() /2;
-    return `rotate(${degrees}deg)`;
+    const minutes = this.currentTime.getMinutes();
+    const hourDegrees = (hours * 30) + (0.5 * minutes);
+    return `rotate(${hourDegrees}deg)`;
   }
 
   get minuteHandTransform() {
-    const degrees = this.currentTime.getMinutes() * 6 ;
-    return `rotate(${degrees}deg)`;
-  }
+    const minutes = this.currentTime.getMinutes();
+    const seconds = this.currentTime.getSeconds();
+    const minuteDegrees = (minutes * 360 / 60) + (seconds * 6 / 60);
+    return `rotate(${minuteDegrees}deg)`;
+}
 
   get secondHandTransform() {
-    const degrees = this.currentTime.getSeconds() * 6;
-    return `rotate(${degrees}deg)`;
+    const seconds = this.currentTime.getSeconds();
+    const secondDegrees = seconds * 6;
+    return `rotate(${secondDegrees}deg)`;
   }
 
   ngOnDestroy() {
