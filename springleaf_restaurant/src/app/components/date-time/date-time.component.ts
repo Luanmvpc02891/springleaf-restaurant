8fa5e5df788e5131680ce1d2db33bce50bfb3508
Lê Trung Hiếu <hieult808@gmail.com>
2023-10-24 12:47:46 +0700

    Update css

--- springleaf_restaurant/src/app/components/date-time/date-time.component.ts
+++ springleaf_restaurant/src/app/components/date-time/date-time.component.ts
@@ -11,6 +11,7 @@ export class DateTimeComponent {
   dateTimeWorker: Worker;
   currentTime: Date = new Date();
   hours: number[] = Array.from({ length: 12 }, (_, index) => index + 1);
+  seconds : number[] = Array.from({ length: 60 }, (_, index) => index + 1);
   //hourDegrees: number = 0;
   //minuteDegrees: number = 0;
   //secondDegrees: number = 0;
@@ -21,28 +22,28 @@ export class DateTimeComponent {
 
   ngOnInit() {
 
-    //this.dateTimeWorker.postMessage('start');
+    this.dateTimeWorker.postMessage('start');
 
-    //this.dateTimeWorker.onmessage = (event) => {
-    //  this.currentTime = event.data;
-    //};
+    this.dateTimeWorker.onmessage = (event) => {
+     this.currentTime = event.data;
+    };
   }
 
-  // get hourHandTransform() {
-  //   const hours = this.currentTime.getHours() % 12;
-  //   const degrees = hours * 30 + this.currentTime.getMinutes() * 0.5;
-  //   return `rotate(${degrees}deg)`;
-  // }
+  get hourHandTransform() {
+    const hours = this.currentTime.getHours() % 12;
+    const degrees = hours * 30 + this.currentTime.getMinutes() /2;
+    return `rotate(${degrees}deg)`;
+  }
 
-  // get minuteHandTransform() {
-  //   const degrees = this.currentTime.getMinutes() * 6 + this.currentTime.getSeconds() * 0.1;
-  //   return `rotate(${degrees}deg)`;
-  // }
+  get minuteHandTransform() {
+    const degrees = this.currentTime.getMinutes() * 6 ;
+    return `rotate(${degrees}deg)`;
+  }
 
-  // get secondHandTransform() {
-  //   const degrees = this.currentTime.getSeconds() * 6;
-  //   return `rotate(${degrees}deg)`;
-  // }
+  get secondHandTransform() {
+    const degrees = this.currentTime.getSeconds() * 6;
+    return `rotate(${degrees}deg)`;
+  }
 
   ngOnDestroy() {
     this.dateTimeWorker.terminate(); // Khi component bị hủy, dừng Web Worker
