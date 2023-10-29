d0d340df192c36e2679f1c507bedb0377b6262cd
maiquocbao666 <maiquocbao666@gmail.com>
2023-10-29 20:01:15 +0700

    Hiển thị component giờ, xóa đồng hồ

--- springleaf_restaurant/src/app/components/date-time/date-time.component.ts
+++ springleaf_restaurant/src/app/components/date-time/date-time.component.ts
@@ -9,10 +9,7 @@ import { DateTimeService } from 'src/app/services/date-time.service';
 })
 export class DateTimeComponent {
 
-  private currentTime: Date = new Date();
-
-  hours: number[] = Array.from({ length: 12 }, (_, index) => index + 1);
-  seconds: number[] = Array.from({ length: 60 }, (_, index) => index + 1);
+  currentDateTime: Date = new Date();
 
   constructor(private dateTimeService: DateTimeService) {
 
@@ -20,30 +17,10 @@ export class DateTimeComponent {
 
   ngOnInit() {
 
-    this.dateTimeService.getCurrentTime().subscribe((time: Date) => {
-      this.currentTime = time;
+    this.dateTimeService.getCurrentTime().subscribe((dateTime: Date) => {
+      this.currentDateTime = dateTime;
     });
 
-  }
-
-  get hourHandTransform() {
-    const hours = this.currentTime.getHours() % 12;
-    const minutes = this.currentTime.getMinutes();
-    const hourDegrees = (hours * 30) + (minutes * 0.5);
-    return `rotate(${hourDegrees}deg)`;
-  }
-
-  get minuteHandTransform() {
-    const minutes = this.currentTime.getMinutes();
-    const seconds = this.currentTime.getSeconds();
-    const minuteDegrees = (minutes * 6) + (seconds * 0.1);
-    return `rotate(${minuteDegrees}deg)`;
-  }
-
-  get secondHandTransform() {
-    const seconds = this.currentTime.getSeconds();
-    const secondDegrees = seconds * 6;
-    return `rotate(${secondDegrees}deg)`;
   }
 
 }
